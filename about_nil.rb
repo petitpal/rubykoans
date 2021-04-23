5f924c2832b0680982c3c4013934f15c93f3d2e5
Paul Newman <paul.newman@teamsoftware.com>
2021-04-23 15:01:08 +0100

    Leaning about nil and object refs

--- about_nil.rb
+++ about_nil.rb
@@ -22,9 +22,16 @@ class AboutNil < Neo::Koan
   end
 
   def test_nil_has_a_few_methods_defined_on_it
-    assert_equal __, nil.nil?
-    assert_equal __, nil.to_s
-    assert_equal __, nil.inspect
+    assert_equal true, nil.nil?
+    assert_equal "", nil.to_s
+    assert_equal "nil", nil.inspect
+
+    # assert_equal true, @blah.nil?
+    # if @blah == nil
+    #   assert true      
+    # else
+    #   assert false
+    # end
 
     # THINK ABOUT IT:
     #
